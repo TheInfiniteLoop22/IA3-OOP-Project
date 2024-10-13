240691e51be6d7fe34071ff86238f6cffb304670
RITANSHU KUMAR <156397435+RITANSHUKUMAR@users.noreply.github.com>
2024-10-14 01:42:52 +0530

    comments added

--- HMSProject/model/Treatment.java
+++ HMSProject/model/Treatment.java
@@ -3,7 +3,7 @@ package model;
 public class Treatment {
     private String description;
     private double cost;
-
+//no need of cost....instead give discount percentage according to schemes
     public Treatment(String description, double cost) {
         this.description = description;
         this.cost = cost;
